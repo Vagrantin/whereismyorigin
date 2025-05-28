fd27add113f2ad4a550d7ff59c52249c872c0d7c
Matthieu Ducorps <sr.ht.qualify064@passmail.net>
2025-05-28 15:16:42 +0900

    switch to working crate for windows cross compile

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 // build.rs
 
-extern crate winres;
+extern crate winresource;
 
 fn main() {
-    let mut res = winres::WindowsResource::new();
+    let mut res = winresource::WindowsResource::new();
     res.set("FileDescription", "Find Origin/Precharge in MXF");
     res.set("ProductName", "Where is my Origin");
     res.set("LegalCopyright", "© 2025 Dalet - Matthieu Ducorps");
